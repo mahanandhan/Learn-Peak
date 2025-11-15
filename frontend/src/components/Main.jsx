baf01925dade9f0572357e8997c12810ae83a622
mahanandhan <mahanandhanmanish@gmail.com>
2025-11-15 19:36:36 +0530

    added the new ui

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
 import Header from './Header'
 
 const Main = () => {
@@ -9,15 +7,12 @@ const Main = () => {
     <div className="flex flex-col min-h-screen bg-gradient-to-r from-pink-100 via-yellow-100 to-green-100 text-gray-800">
       
       {/* Navbar */}
-      <Navbar />
 
       {/* Main Content */}
       <main className="flex-grow bg-gradient-to-r from-pink-100 via-yellow-100 to-green-100">
         <Header />
       </main>
 
-      {/* Footer */}
-      <Footer />
     </div>
   )
 }
